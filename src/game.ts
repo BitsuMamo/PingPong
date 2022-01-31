88ad29912ab9df56657813810a6987ac139caa72
Bitsu Mamo <bitsua.mamo@gmail.com>
2022-01-31 19:43:49 +0300

    Finished adding keyboard controller and score checking.

--- src/game.ts
+++ src/game.ts
@@ -8,36 +8,38 @@ type Speed = {
   y: number
 }
 
-const barDimension = {
-  width: window.innerWidth / 40,
-  height: window.innerHeight / 5
+const WIN_SCORE = 5;
+
+const BAR_DIMENSION = {
+  width: window.innerWidth / 70,
+  height: window.innerHeight / 4
 }
 
-const ballRadius = 40;
+const BALL_RADIUS = 30;
 
-const border = {
+const BORDER = {
   top: 0,
   right: window.innerWidth,
   bottom: window.innerHeight,
   left: 0,
 }
 
-const left_bar_position = {
+const LEFT_BAR_POSITION = {
   x: 0,
-  y: window.innerHeight / 2 - barDimension.height / 2
+  y: window.innerHeight / 2 - BAR_DIMENSION.height / 2
 }
 
-const right_bar_position = {
-  x: window.innerWidth - barDimension.width,
-  y: window.innerHeight / 2 - barDimension.height / 2
+const RIGHT_BAR_POSITION = {
+  x: window.innerWidth - BAR_DIMENSION.width,
+  y: window.innerHeight / 2 - BAR_DIMENSION.height / 2
 }
 
-const ball_position = {
-  x: window.innerWidth / 2 - ballRadius / 2,
-  y: window.innerHeight / 2 - ballRadius / 2
+const BALL_POSITION = {
+  x: window.innerWidth / 2 - BALL_RADIUS / 2,
+  y: window.innerHeight / 2 - BALL_RADIUS / 2
 }
 
-const ball_speed = {
+const BALL_SPEED = {
   x: -10,
   y: 0
 }
@@ -65,14 +67,14 @@ class Player {
     }
 
     this.paddleUp = () => {
-      if (position.y >= border.top) {
+      if (position.y >= BORDER.top) {
         position.y -= this.speed;
         this.draw();
       }
     }
 
     this.paddleDown = () => {
-      if (position.y <= border.bottom - barDimension.height) {
+      if (position.y <= BORDER.bottom - BAR_DIMENSION.height) {
         position.y += this.speed;
         this.draw();
       }
@@ -91,9 +93,9 @@ class AI extends Player {
     this.ball_position = ball_position;
 
     this.calculate_postion = () => {
-      if (this.ball_position.y + ballRadius > this.position.y + barDimension.height && this.position.y <= border.bottom - barDimension.height) {
+      if (this.ball_position.y + BALL_RADIUS > this.position.y + BAR_DIMENSION.height && this.position.y <= BORDER.bottom - BAR_DIMENSION.height) {
         this.position.y += this.speed;
-      } else if (this.ball_position.y - ballRadius < this.position.y + barDimension.height && this.position.y >= border.top) {
+      } else if (this.ball_position.y - BALL_RADIUS < this.position.y + BAR_DIMENSION.height && this.position.y >= BORDER.top) {
         this.position.y -= this.speed;
       }
 
@@ -109,8 +111,8 @@ class Ball {
     y: 10
   };
   position: Position = {
-    x: window.innerWidth / 2 - ballRadius / 2,
-    y: window.innerHeight / 2 - ballRadius / 2,
+    x: window.innerWidth / 2 - BALL_RADIUS / 2,
+    y: window.innerHeight / 2 - BALL_RADIUS / 2,
   }
   draw;
   reset_position;
@@ -158,32 +160,32 @@ class Collision {
     this.right_player = right_player;
 
     this.ballPaddleCollision = () => {
-      if (ball.position.x <= (left_player.position.x + barDimension.width) + ballRadius / 2) {
-        if (ball.position.y >= left_player.position.y && ball.position.y <= left_player.position.y + barDimension.height) {
+      if (ball.position.x <= (left_player.position.x + BAR_DIMENSION.width) + BALL_RADIUS / 2) {
+        if (ball.position.y >= left_player.position.y && ball.position.y <= left_player.position.y + BAR_DIMENSION.height) {
           ball.speed.x *= -1;
         }
       }
 
-      if (ball.position.x >= (right_player.position.x - barDimension.width) - ballRadius / 2) {
-        if (ball.position.y >= right_player.position.y && ball.position.y <= right_player.position.y + barDimension.height) {
+      if (ball.position.x >= (right_player.position.x - BAR_DIMENSION.width) - BALL_RADIUS / 2) {
+        if (ball.position.y >= right_player.position.y && ball.position.y <= right_player.position.y + BAR_DIMENSION.height) {
           ball.speed.x *= -1;
         }
       }
     }
 
     this.ballBorderCollision = () => {
-      if (ball.position.y < border.top || ball.position.y >= border.bottom - ballRadius) {
+      if (ball.position.y < BORDER.top || ball.position.y >= BORDER.bottom - BALL_RADIUS) {
         ball.speed.y *= -1;
       }
 
-      if (ball.position.x < border.left){
+      if (ball.position.x < BORDER.left) {
         right_player.score += 1;
-        ball.reset_position(ball_position);
+        ball.reset_position(BALL_POSITION);
       }
 
-      if (ball.position.x > border.right){
+      if (ball.position.x > BORDER.right) {
         left_player.score += 1;
-        ball.reset_position(ball_position);
+        ball.reset_position(BALL_POSITION);
       }
 
     }
@@ -199,38 +201,101 @@ class Game {
   right_bar_score = document.getElementById('right_bar_score')!;
   ball = document.getElementById('ball')!;
   playerMode: string;
+  check_score;
 
   start;
   update;
+  controller: any;
+  executePress;
 
   constructor() {
-    let ball = new Ball(this.ball);
-    // let left_player = new Player(10, this.left_bar, left_bar_position, this.left_bar_score);
-    let left_player = new AI(10, this.left_bar, left_bar_position, this.left_bar_score, ball.position);
-    // let right_player = new Player(10, this.right_bar, right_bar_position, this.right_bar_score);
-    let right_player = new AI(10, this.right_bar, right_bar_position, this.right_bar_score, ball.position);
-    let collision = new Collision(ball, left_player, right_player);
+
+    this.controller = {
+      87: { pressed: false, func: undefined },
+      83: { pressed: false, func: undefined },
+      38: { pressed: false, func: undefined },
+      40: { pressed: false, func: undefined },
+    }
+
+    this.executePress = () => {
+      Object.keys(this.controller).forEach(key => {
+        this.controller[key].pressed && this.controller[key].func();
+      })
+    }
+
+    this.left_bar.style.width = BAR_DIMENSION.width + 'px';
+    this.left_bar.style.height = BAR_DIMENSION.height + 'px';
+
+    this.right_bar.style.width = BAR_DIMENSION.width + 'px';
+    this.right_bar.style.height = BAR_DIMENSION.height + 'px';
+
+    this.ball.style.width = BALL_RADIUS + 'px';
+    this.ball.style.height = BALL_RADIUS + 'px';
 
     this.playerMode = sessionStorage.getItem('playerMode')!;
 
-    document.addEventListener('keydown', (e) => {
-      switch (e.keyCode) {
-        case 87:
-          left_player.paddleUp();
-          break;
-        case 83:
-          left_player.paddleDown();
-          break;
-        case 38:
-          right_player.paddleUp();
-          break;
-        case 40:
-          right_player.paddleDown();
-          break;
+    let ball = new Ball(this.ball);
+    let left_player: any, right_player: any;
+    switch (this.playerMode) {
+      case 'onePlayer':
+        left_player = new Player(10, this.left_bar, LEFT_BAR_POSITION, this.left_bar_score);
+        right_player = new AI(10, this.right_bar, RIGHT_BAR_POSITION, this.right_bar_score, ball.position);
+
+        this.controller[87] = { pressed: false, func: left_player.paddleUp };
+        this.controller[83] = { pressed: false, func: left_player.paddleDown };
+
+        break;
+      case 'twoPlayer':
+        left_player = new Player(10, this.left_bar, LEFT_BAR_POSITION, this.left_bar_score);
+        right_player = new Player(10, this.right_bar, RIGHT_BAR_POSITION, this.right_bar_score);
+
+        this.controller[87] = { pressed: false, func: left_player.paddleUp };
+        this.controller[83] = { pressed: false, func: left_player.paddleDown };
+        this.controller[38] = { pressed: false, func: right_player.paddleUp };
+        this.controller[40] = { pressed: false, func: right_player.paddleDown };
+
+        break;
+      case 'noPlayer':
+        left_player = new AI(10, this.left_bar, LEFT_BAR_POSITION, this.left_bar_score, ball.position);
+        right_player = new AI(10, this.right_bar, RIGHT_BAR_POSITION, this.right_bar_score, ball.position);
+        break;
+      default:
+        left_player = new Player(10, this.left_bar, LEFT_BAR_POSITION, this.left_bar_score);
+        right_player = new Player(10, this.right_bar, RIGHT_BAR_POSITION, this.right_bar_score);
+        break;
+    }
+
+    this.check_score = () => {
+      if (left_player.score >= WIN_SCORE) {
+        alert("Left Player Wins");
+        ball.reset_position(BALL_POSITION);
+        left_player.score = 0;
+        right_player.score = 0;
+      }
+      if (right_player.score >= WIN_SCORE) {
+        alert("Right Player Wins");
+        ball.reset_position(BALL_POSITION);
+        left_player.score = 0;
+        right_player.score = 0;
+      }
+    }
+
+    let collision = new Collision(ball, left_player, right_player);
+
+
+    document.addEventListener("keydown", (e) => {
+      if (this.controller[e.keyCode]) {
+        this.controller[e.keyCode].pressed = true;
       }
     })
 
-    
+    document.addEventListener("keyup", (e) => {
+      if (this.controller[e.keyCode]) {
+        this.controller[e.keyCode].pressed = false;
+      }
+    })
+
+
     this.start = () => {
       requestAnimationFrame(this.update);
     }
@@ -243,29 +308,34 @@ class Game {
       deltaTime = (timestamp - lastTimestamp) / perfectFrameTime;
       lastTimestamp = timestamp;
 
-      switch(this.playerMode){
+      // Collision, drawing, and ball movment needed invariably.
+      collision.ballBorderCollision();
+      collision.ballPaddleCollision();
+      ball.move();
+      ball.draw();
+      left_player.draw();
+      right_player.draw();
+      // Movement calculation needed depending on the player type(AI, Player)
+      switch (this.playerMode) {
         case 'onePlayer':
-          collision.ballBorderCollision();
-          collision.ballPaddleCollision();
-          ball.move();
-          ball.draw();
-          left_player.calculate_postion();
           right_player.calculate_postion();
-          left_player.draw();
-          right_player.draw();
           break;
         case 'twoPlayer':
-          collision.ballBorderCollision();
-          collision.ballPaddleCollision();
-          ball.move();
-          ball.draw();
-          left_player.draw();
-          right_player.draw();
+          break;
+        case 'noPlayer':
+          left_player.calculate_postion();
+          right_player.calculate_postion();
+          break;
+        default:
+          right_player.calculate_postion();
           break;
       }
+
+      this.executePress();
+      // Check score on every frame.
+      this.check_score();
     }
   }
-
 
 }
 
